c985f9c43e106058ae47a0e070163651fc142fb9
juampibruno11 <juampiibruno@outlook.com>
2025-10-28 21:21:15 -0300

    xx

--- django2/producto/models.py
+++ django2/producto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class class Producto(models.Model):
+    nombre = models.CharField(max_length=30)
+    precio = models.CharField(max_length=30)
+    fecha = models.CharField(max_length=30)
+    cantidad = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=30)
+    
